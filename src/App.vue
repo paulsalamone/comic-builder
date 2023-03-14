969486d15302933b68765682585aa5cf5d68c531
Paul Salamone <93609282+paulsalamone@users.noreply.github.com>
2023-03-15 00:27:50 +0100

    ROUGH version of abs pos

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,16 @@
     <div class="dugout" dropzone="true" @drop="handleDrop" @dragover="handleDragover">
       <h2>dugout</h2>
       <div class="dropzone" id="dugout">
-        <!-- PLAYERS -->
         <div id="player-1" draggable="true" @dragstart="handleDragstart" @drag="handleDrag">
-          some text
+          player 1
         </div>
       </div>
     </div>
     <div class="field">
-      <h2>field</h2>
       <div class="dropzone" dropzone="true" @drop="handleDrop" @dragover="handleDragover"></div>
+      <h3>Rect: {{ rectPos }}</h3>
+      <h3>Absolute: {{ absPos }}</h3>
+      <h3>Relative: {{ dragPos }}</h3>
     </div>
   </div>
 </template>
@@ -32,21 +33,41 @@ import { ref, computed, onMounted } from 'Vue'
 // DRAG STUFF
 const handleDragstart = (event) => {
   event.dataTransfer.setData('text/plain', event.target.id)
-
   event.dataTransfer.dropEffect = 'move'
 }
 
 const handleDragover = (event) => {
   event.preventDefault()
   event.dataTransfer.dropEffect = 'move'
+
+  const el = document.getElementById('player-1')
+  const p1a = Math.floor(el.getBoundingClientRect().left)
+  const p1b = Math.floor(el.getBoundingClientRect().top)
+
+  console.log(`cursor: ${event.clientX}, ${event.clientY}`)
+  console.log(
+    `boxsor: ${Math.floor(el.getBoundingClientRect().left)}, ${Math.floor(
+      el.getBoundingClientRect().top
+    )}`
+  )
 }
 
 const handleDrop = (event) => {
   event.preventDefault()
-
   const data = event.dataTransfer.getData('text/plain')
-
   event.target.appendChild(document.getElementById(data))
+
+  const el = document.getElementById('player-1')
+
+  console.log(
+    `NEW boxsor: ${Math.floor(el.getBoundingClientRect().left)}, ${Math.floor(
+      el.getBoundingClientRect().top
+    )}`
+  )
+  el.style.position = 'absolute'
+  el.style.left = event.clientX - Math.floor(el.getBoundingClientRect().left) + 'px'
+  el.style.top = event.clientY - Math.floor(el.getBoundingClientRect().top) + 'px'
+  // console.log('XXXX: ', el.style.left, el.style.top)
 }
 </script>
 
